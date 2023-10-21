2e702320d3d2ee12c724e90fd555c74d5d7657de
phil <philjke@mail.ru>
2023-10-21 15:36:44 +0700

    add users loging

--- index.js
+++ index.js
@@ -1,12 +1,42 @@
 const TelegramBot = require('node-telegram-bot-api');
-const token = '6934682720:AAFGX4zxJBwUcz07tuGSVTKhN6jgH76k1ss'; // Замените на свой API-токен
+const fs = require('fs'); // Подключаем модуль для работы с файловой системой
+const nodemailer = require('nodemailer');
+const token = '6934682720:AAFGX4zxJBwUcz07tuGSVTKhN6jgH76k1ss';
+
+// Настройки для отправки писем
+const transporter = nodemailer.createTransport({
+	service: 'smtp.yandex.ru',
+	port: 465, // Укажите адрес вашего SMTP-сервера
+	auth: {
+		user: 'gamediagnostic@ya.ru',
+		pass: '1q2w3e_2',
+	},
+});
+
+// Функция для отправки письма на почту
+function sendEmail(subject, text) {
+	const mailOptions = {
+		from: 'gamediagnostic@ya.ru', // Отправитель
+		to: 'philjke@mail.ru', // Получатель
+		subject: subject,
+		text: text,
+	};
+
+	transporter.sendMail(mailOptions, (error, info) => {
+		if (error) {
+			console.error('Ошибка отправки письма:', error);
+		} else {
+			console.log('Письмо успешно отправлено:', info.response);
+		}
+	});
+}
 
 // Варианты сообщений с описаниями
 const messages = [
 	{
 		text: 'Вы всегда помогаете (даже если не просят). Тусуетесь в чужих жизнях, не думаете о себе.',
 		description:
-			"Нам часто кажется, что помогать другим - наша святая обязанность, особенно когда речь идет о родителях, родственниках, близких людях. Помогать из состояния 'я должен' довольно тяжело. А что, если попробовать оказывать помощь из состояния 'Я могу сейчас помочь - я помогаю или Я могу помочь ровно столько сейчас'? А помогают ли вам? Как это происходит - вы просите или ждете помощи молча? Объемная тема для исследования, предлагаю поразмышлять.",
+			'Нам часто кажется, что помогать другим - наша святая обязанность, особенно когда речь идет о родителях, родственниках, близких людях. Помогать из состояния "я должен" довольно тяжело. А что, если попробовать оказывать помощь из состояния "Я могу сейчас помочь - я помогаю или Я могу помочь ровно столько сейчас"? А помогают ли вам? Как это происходит - вы просите или ждете помощи молча? Объемная тема для исследования, предлагаю поразмышлять.',
 	},
 	{
 		text: 'Вы давно не отдыхали, накопили хроническую усталость.',
@@ -20,8 +50,60 @@ const messages = [
 	},
 ];
 
+// Функция для сохранения сообщения пользователя и его никнейма в файл
+function saveUserMessage(userId, username, messageText) {
+	// Прочитаем текущее содержимое файла users.json
+	fs.readFile('users.json', (err, data) => {
+		if (err) {
+			console.error('Ошибка чтения файла users.json:', err);
+			return;
+		}
+
+		let users = JSON.parse(data); // Разбираем JSON в массив пользователей
+
+		// Создаем объект с данными о сообщении
+		const userMessage = {
+			userId: userId,
+			username: username,
+			messageText: messageText,
+			timestamp: new Date().getTime(), // Добавляем временную метку
+		};
+
+		users.push(userMessage); // Добавляем объект в массив пользователей
+
+		// Записываем обновленный массив пользователей обратно в файл
+		fs.writeFile('users.json', JSON.stringify(users, null, 2), (err) => {
+			if (err) {
+				console.error('Ошибка записи файла users.json:', err);
+			}
+		});
+	});
+
+	const subject = 'Новое сообщение от пользователя ' + username;
+	const text = `Пользователь ${username} (ID: ${userId}) отправил следующее сообщение:\n\n${messageText}`;
+
+	sendEmail(subject, text);
+}
+
+function sendWelcomeMessage(chatId) {
+	const welcomeMessage =
+		'Меня зовут Оксана Гаврилиди, я практикующий психолог. Более года я игротерапевт - тот самый человек, который ведет трансформационные игры с терапевтическим эффектом. В этом боте вы можете пройти короткую диагностику, которая позволит посмотреть на ваш запрос с помощью игры.';
+	bot.sendMessage(chatId, welcomeMessage);
+
+	// Отправляем второе сообщение через 1 секунду
+	setTimeout(() => {
+		const secondMessage =
+			'Сформулируйте, пожалуйста, ваш запрос. Он может касаться любой сферы вашей жизни (отношения, доход, реализация, отдых и другие).';
+		bot.sendMessage(chatId, secondMessage);
+		// Переключаем состояние на ожидание новых сообщений
+		states.waitingForAnswer = true;
+	}, 1000);
+}
+
 // Состояния бота
-const states = {};
+const states = {
+	waitingForAnswer: false,
+};
 
 // Инициализируем бота
 const bot = new TelegramBot(token, { polling: true });
@@ -30,25 +112,24 @@ const bot = new TelegramBot(token, { polling: true });
 bot.onText(/\/start/, (msg) => {
 	const chatId = msg.chat.id;
 	const userId = msg.from.id;
-
+	states.waitingForAnswer = true;
 	// Если нет состояния для пользователя, устанавливаем начальное состояние
-	if (!states[userId]) {
+	if (!states[userId] && states.waitingForAnswer) {
 		states[userId] = 'start';
-		const welcomeMessage =
-			'Меня зовут Оксана Гаврилиди, я практикующий психолог. Более года я игротерапевт - тот самый человек, который ведет трансформационные игры с терапевтическим эффектом. В этом боте вы можете пройти короткую диагностику, которая позволит посмотреть на ваш запрос с помощью игры.';
-		bot.sendMessage(chatId, welcomeMessage);
+		sendWelcomeMessage(chatId); // Вызываем функцию для отправки приветственного сообщения
+	}
+});
 
-		// Отправляем второе сообщение через 1 секунду
-		setTimeout(() => {
-			const secondMessage =
-				'Сформулируйте, пожалуйста, ваш запрос. Он может касаться любой сферы вашей жизни (отношения, доход, реализация, отдых и другие).';
-			bot.sendMessage(chatId, secondMessage);
-			// Переключаем состояние на ожидание новых сообщений
-			states[userId] = 'waiting';
-		}, 1000);
-	} else {
-		// Если пользователь уже начал диалог, игнорируем команду /start
-		bot.sendMessage(chatId, 'Вы уже начали диалог. Пожалуйста, отправьте ваш запрос.');
+// Обработчик команды /restart
+bot.onText(/\/restart/, (msg) => {
+	const chatId = msg.chat.id;
+	const userId = msg.from.id;
+
+	// Проверяем состояние, чтобы исключить выполнение команды /restart в определенных ситуациях
+	if (states.waitingForAnswer) {
+		// Если нет состояния для пользователя, устанавливаем начальное состояние
+		states[userId] = 'start';
+		sendWelcomeMessage(chatId); // Вызываем функцию для отправки приветственного сообщения
 	}
 });
 
@@ -56,64 +137,43 @@ bot.onText(/\/start/, (msg) => {
 bot.on('message', (msg) => {
 	const userId = msg.from.id;
 
-	if (states[userId] === 'waiting') {
-		const chatId = msg.chat.id;
+	if (states.waitingForAnswer) {
+		if (msg.text === '/restart' || msg.text === '/start') {
+			return;
+		} else {
+			const chatId = msg.chat.id;
 
-		// Получаем выбранный вариант ответа
-		const selectedMessage = messages[Math.floor(Math.random() * messages.length)];
+			const userId = msg.from.id;
+			const username = msg.from.username || 'Без никнейма'; // Если у пользователя нет никнейма, используем "Без никнейма"
+			const messageText = msg.text;
 
-		// Формируем сообщение с кнопкой "ПОДРОБНЕЕ"
-		const options = {
-			reply_markup: {
-				inline_keyboard: [[{ text: 'ПОДРОБНЕЕ', callback_data: 'description' }]],
-			},
-		};
+			// Сохраняем сообщение пользователя в файл
+			saveUserMessage(userId, username, messageText);
 
-		// Отправляем сообщение с кнопкой
-		bot.sendMessage(chatId, selectedMessage.text, options);
+			// Получаем выбранный вариант ответа
+			const selectedMessage = messages[Math.floor(Math.random() * messages.length)];
 
-		// Сохраняем выбранный вариант в состоянии
-		states[userId] = 'description';
+			// Формируем сообщение с кнопкой "ПОДРОБНЕЕ"
+			const options = {
+				reply_markup: {
+					inline_keyboard: [[{ text: 'ПОДРОБНЕЕ', callback_data: 'description' }]],
+				},
+			};
 
-		// Сохраняем подробное описание в состоянии
-		states[userId + '_description'] = selectedMessage.description;
-	}
-});
+			// Отправляем сообщение с кнопкой
+			bot.sendMessage(chatId, selectedMessage.text, options);
 
-// Отслеживаем нажатие кнопки "ПОДРОБНЕЕ"
-// Отслеживаем все остальные сообщения
-bot.on('message', (msg) => {
-	const userId = msg.from.id;
+			// Сохраняем выбранный вариант в состоянии
+			states[userId] = 'description';
 
-	if (states[userId] === 'waiting') {
-		const chatId = msg.chat.id;
-
-		// Получаем выбранный вариант ответа
-		const selectedMessage = messages[Math.floor(Math.random() * messages.length)];
-
-		// Формируем сообщение с кнопками "ПОДРОБНЕЕ" и "ДАЛЕЕ"
-		const options = {
-			reply_markup: {
-				inline_keyboard: [
-					[{ text: 'ПОДРОБНЕЕ', callback_data: 'description' }],
-					[{ text: 'ДАЛЕЕ', callback_data: 'next' }],
-				],
-			},
-		};
-
-		// Отправляем сообщение с кнопками
-		bot.sendMessage(chatId, selectedMessage.text, options);
-
-		// Сохраняем выбранный вариант в состоянии
-		states[userId] = 'description';
-
-		// Сохраняем подробное описание в состоянии
-		states[userId + '_description'] = selectedMessage.description;
+			// Сохраняем подробное описание в состоянии
+			states[userId + '_description'] = selectedMessage.description;
+			states.waitingForAnswer = false;
+		}
 	}
 });
 
 // Отслеживаем нажатие кнопок "ПОДРОБНЕЕ" и "ДАЛЕЕ"
-
 bot.on('callback_query', (query) => {
 	const userId = query.from.id;
 	const chatId = query.message.chat.id;
@@ -123,8 +183,7 @@ bot.on('callback_query', (query) => {
 		(states[userId] === 'description' || states[userId] === 'further')
 	) {
 		if (states[userId] === 'description') {
-			// Получаем выбранный вариант ответа
-			const selectedMessage = messages[Math.floor(Math.random() * messages.length)];
+			const description = states[userId + '_description']; // Получаем сохраненное описание из состояния
 
 			// Формируем сообщение с кнопкой "ДАЛЕЕ"
 			const options = {
@@ -134,7 +193,7 @@ bot.on('callback_query', (query) => {
 			};
 
 			// Отправляем сообщение с подробным описанием и кнопкой "ДАЛЕЕ"
-			bot.sendMessage(chatId, selectedMessage.description, options);
+			bot.sendMessage(chatId, description, options);
 
 			// Сохраняем состояние "description" для последующего отслеживания нажатия "ДАЛЕЕ"
 			states[userId] = 'description';
@@ -154,7 +213,7 @@ bot.on('callback_query', (query) => {
 			bot.answerCallbackQuery(query.id);
 
 			// Затем переключаем состояние на ожидание новых сообщений
-			states[userId] = 'waiting';
+			states.waitingForAnswer = false;
 		}
 	} else if (query.data === 'further' && states[userId] === 'description') {
 		// Формируем сообщение "Узнать, какие ресурсы помогут мне решить мой запрос?" с кнопкой "ДА"
@@ -172,18 +231,15 @@ bot.on('callback_query', (query) => {
 		bot.answerCallbackQuery(query.id);
 
 		// Затем переключаем состояние на ожидание новых сообщений
-		states[userId] = 'waiting';
 	}
 });
-
-// Отслеживаем нажатие кнопки "ДА"
 
 // Отслеживаем нажатие кнопки "ДА"
 bot.on('callback_query', (query) => {
 	const userId = query.from.id;
 	const chatId = query.message.chat.id;
 
-	if (query.data === 'yes' && states[userId] === 'waiting') {
+	if (query.data === 'yes') {
 		// Генерируем два случайных индекса для выбора пунктов из списка
 		const availableResources = [
 			'ЗДОРОВЬЕ: к этому ресурсу можно отнести состояние и физического и ментального здоровья. Что с телом? Что с эмоциональным состоянием? Чего не хватает? ',
@@ -215,14 +271,14 @@ bot.on('callback_query', (query) => {
 		bot.sendMessage(chatId, responseMessage, options);
 
 		// Затем переключаем состояние на ожидание новых сообщений
-		states[userId] = 'waiting';
+		states.waitingForAnswer = false;
 	}
 	// Отслеживаем нажатие кнопки "Далее"
 	// Отслеживаем нажатие кнопки "Далее"
-	else if (query.data === 'further_details' && states[userId] === 'waiting') {
+	else if (query.data === 'further_details') {
 		// Формируем сообщение "Более подробно исследовать ваш запрос можно при помощи игры. чтобы узнать расписание ближайших игр"
 		const detailsMessage =
-			'Более подробно исследовать ваш запрос можно при помощи игры. Чтобы узнать расписание ближайших игр, свяжитесь с нами.';
+			'Более подробно исследовать ваш запрос можно при помощи игры. Чтобы узнать расписание ближайших игр, свяжитесь со мной.';
 
 		const options = {
 			reply_markup: {
@@ -234,6 +290,10 @@ bot.on('callback_query', (query) => {
 		bot.sendMessage(chatId, detailsMessage, options);
 
 		// Затем переключаем состояние на ожидание новых сообщений
-		states[userId] = 'waiting';
+		states.waitingForAnswer = true;
 	}
+});
+
+bot.on('polling_error', (error) => {
+	console.log('Polling error:', error);
 });
